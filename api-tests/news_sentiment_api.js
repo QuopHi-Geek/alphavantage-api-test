8faec9d82dd9798f1d4e5b99d493a52132cb8422
Wisdom Tetteh <tettehwisdom.itil@gmail.com>
2024-04-21 19:24:14 +0200

    adds extra assertions

--- api-tests/news_sentiment_api.js
+++ api-tests/news_sentiment_api.js
@@ -8,7 +8,7 @@ const FUNCTION = "NEWS_SENTIMENT";
 //### TEST CASES FOR NEWS SENTIMENT API ###
 // 1. Test a valid request to the News Sentiment API
 // 2. Test the News Sentiment API with an missing or invalid API key
-// 3. Test the News Sentiment API with missing required parameters
+// 3. Test the News Sentiment API with missing/invalid required parameters
 
 
 describe("News Sentiment API", () => {
@@ -18,12 +18,38 @@ describe("News Sentiment API", () => {
     const request = await axios.get(
       `${BASE_URL}/query?function=${FUNCTION}&tickers=${tickers}&outputsize=full&apikey=demo&apiKey=${API_KEY}`
     );
-    const response = request.data;
 
-    // Check that the response has a sentiment score
+    const response = request;
+    //default header values
+    const contentType = response.headers["content-type"];
+    const block = (axios.defaults.headers.common["X-XSS-Protection"] =
+      "1; mode=block");
+    const deny = (response.headers["X-Frame-Options"] = "DENY");
+    const nosniff = (axios.defaults.headers.common["X-Content-Type-Options"] =
+      "nosniff");
+    const strict = (axios.defaults.headers.common["Strict-Transport-Security"] =
+      "max-age=31536000; includeSubDomains");
+
     expect(response).to.be.an("object");
-    assert.equal(response["Error Message"], null);
 
+    assert.equal(response.status, 200); //response status code is 200
+
+    // Check Content-Type header is set to "application/json"
+    assert.equal(contentType, "application/json");
+
+    // Check that the X-XSS-Protection header is set to "1; mode=block"
+    assert.equal(block, "1; mode=block");
+
+    // Check that the X-Frame-Options header is set to "DENY"
+    assert.equal(deny, "DENY");
+
+    // Check that the X-Content-Type-Options header is set to "nosniff"
+    assert.equal(nosniff, "nosniff");
+
+    // Check the Strict transport setting
+    assert.equal(strict, "max-age=31536000; includeSubDomains");
+
+    assert.equal(response["Error Message"], null);
   });
 
 
@@ -31,13 +57,37 @@ describe("News Sentiment API", () => {
   it("Get news articles with missing or invalid API key", async () => {
     let tickers = "AAPL"; //  AAPL,GOOG,MSFT
     let API_KEY = "";
+
     const request = await axios.get(
       `${BASE_URL}/query?function=${FUNCTION}&tickers=${tickers}&outputsize=full&apiKey=${API_KEY}`
     );
-    const response = request;
 
-    // Check that the response has an error message
-    expect(response.status).to.equal(200);
+    const response = request;
+    //default header values
+    const contentType = response.headers["content-type"];
+    const block = (axios.defaults.headers.common["X-XSS-Protection"] =
+      "1; mode=block");
+    const deny = (response.headers["X-Frame-Options"] = "DENY");
+    const nosniff = (axios.defaults.headers.common["X-Content-Type-Options"] =
+      "nosniff");
+    const strict = (axios.defaults.headers.common["Strict-Transport-Security"] =
+      "max-age=31536000; includeSubDomains");
+
+    expect(response).to.be.an("object");
+    assert.equal(response.status, 200); //response status code is 200
+
+    // Check Content-Type header is set to "application/json"
+    assert.equal(contentType, "application/json");
+
+    // Check that the X-XSS-Protection header is set to "1; mode=block"
+    assert.equal(block, "1; mode=block");
+
+    // Check that the X-Frame-Options header is set to "DENY"
+    assert.equal(deny, "DENY");
+
+    // Check that the X-Content-Type-Options header is set to "nosniff"
+    assert.equal(nosniff, "nosniff");
+
     assert(!response.data.error);
 
     assert.property(response.data, "Error Message");
@@ -48,16 +98,43 @@ describe("News Sentiment API", () => {
   });
 
 
-  it("Get news articles with invalid news with missing required parameters", async () => {
+
+
+  it("Get news articles with invalid news with missing/invalid required parameters", async () => {
     let FUNCTION = "NEWS_ERROR";
     let tickers = "";
 
-    const request = await axios.get(`${BASE_URL}/query?&function=${FUNCTION}&tickers=${tickers}&outputsize=full&apiKey=${API_KEY}`);
+    const request = await axios.get(
+      `${BASE_URL}/query?&function=${FUNCTION}&tickers=${tickers}&outputsize=full&apiKey=${API_KEY}`
+    );
 
     const response = request;
-    // Check that the response has an error message
+    //default header values
+    const contentType = response.headers["content-type"];
+    const block = (axios.defaults.headers.common["X-XSS-Protection"] =
+      "1; mode=block");
+    const deny = (response.headers["X-Frame-Options"] = "DENY");
+    const nosniff = (axios.defaults.headers.common["X-Content-Type-Options"] =
+      "nosniff");
+    const strict = (axios.defaults.headers.common["Strict-Transport-Security"] =
+      "max-age=31536000; includeSubDomains");
+
+    expect(response).to.be.an("object");
+    assert.equal(response.status, 200); //response status code is 200
+
+    // Check Content-Type header is set to "application/json"
+    assert.equal(contentType, "application/json");
+
+    // Check that the X-XSS-Protection header is set to "1; mode=block"
+    assert.equal(block, "1; mode=block");
+
+    // Check that the X-Frame-Options header is set to "DENY"
+    assert.equal(deny, "DENY");
+
+    // Check that the X-Content-Type-Options header is set to "nosniff"
+    assert.equal(nosniff, "nosniff");
+
     expect(response.status).to.equal(200);
     assert.property(response.data, "Error Message");
   });
-
 });
